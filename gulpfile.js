0f3062e8f7481139289072d5134173595904d635
justinjaywang <justinjaywang@gmail.com>
2015-06-21 14:31:57 -0700

    cleaned up comments in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,24 @@
-var gulp =          require('gulp');
-// var clean =         require('gulp-clean');
-var watch =         require('gulp-watch');
-// var htmlmin =       require('gulp-htmlmin');
-// var concat =        require('gulp-concat');
-// var uglify =        require('gulp-uglify');
-var less =          require('gulp-less');
-// var autoprefixer =  require('gulp-autoprefixer');
-var minifycss =     require('gulp-minify-css');
-var rename =        require('gulp-rename');
-// var cache =         require('gulp-cache');
-// var changed =       require('gulp-changed');
-var util =          require('gulp-util');
+var gulp =      require('gulp');
+var watch =     require('gulp-watch');
+var less =      require('gulp-less');
+var minifycss = require('gulp-minify-css');
+var rename =    require('gulp-rename');
+var util =      require('gulp-util');
 
 var paths = {
-  // markup:     ['source/**/*.html'],
-  // scripts:    ['source/js/**/*.js'],
-  styles:     ['source/less/all.less']
+  // markup:  ['source/**/*.html'],
+  // scripts: ['source/js/**/*.js'],
+  styles:  ['source/less/all.less']
 };
 
 var dest = {
-  build:  ''
+  build: ''
 };
-// dest.markup =     dest.build;
-// dest.scripts =    dest.build + '/js';
-dest.styles =     dest.build;
+// dest.markup =  dest.build;
+// dest.scripts = dest.build + '/js';
+dest.styles =  dest.build;
 
-// // minify HTML
+// // markup
 // gulp.task('markup', function() {
 //   return gulp.src(paths.markup)
 //     // .pipe(htmlify())
@@ -34,7 +27,7 @@ dest.styles =     dest.build;
 //     .on('error', util.log);
 // });
   
-// // minify and copy all JavaScript (except vendor scripts)
+// // scripts
 // gulp.task('scripts', function() {
 //   return gulp.src(paths.scripts)
 //     // .pipe(coffee())
@@ -46,12 +39,10 @@ dest.styles =     dest.build;
 //     .on('error', util.log);
 // });
   
-// compile Less, and minify the resulting CSS
+// styles
 gulp.task('styles', function () {
   return gulp.src(paths.styles)
-    // .pipe(changed(dest.styles)) // exclude unmodified files
     .pipe(less())
-    // .pipe(autoprefixer())
     .pipe(gulp.dest(dest.styles))
     .pipe(minifycss())
     .pipe(rename({extname: '.min.css'}))
@@ -59,28 +50,10 @@ gulp.task('styles', function () {
     .on('error', util.log);
 });
 
-// // copy over miscellaneous files
-// gulp.src('source/fonts/icons.*')
-//   .pipe(gulp.dest('build/fonts'))
-//   .on('error', util.log);
-
-// gulp.src('source/.htaccess')
-//   .pipe(gulp.dest('build'))
-//   .on('error', util.log);
-
-// gulp.src('source/robots.txt')
-//   .pipe(gulp.dest('build'))
-//   .on('error', util.log);
-
-// gulp.src('source/favicon.ico')
-//   .pipe(gulp.dest('build'))
-//   .on('error', util.log);
-  
 // rerun the task when a file changes
 gulp.task('watch', function() {
-  // gulp.watch(paths.markup, ['markup']);
   gulp.watch(['source/less/*.less'], ['styles']);
 });
 
-// default task (called when you run `gulp` from cli)
+// default task, called when you run `gulp`
 gulp.task('default', ['styles', 'watch']);
